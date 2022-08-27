6c59f8b189f9ea90e30c0aeee8610417e7e5a725
Shigma <shigma10826@gmail.com>
2022-08-27 22:49:56 +0800

    feat: add basic implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import env from 'env-paths'
+import { join } from 'path'
+import { spawn, SpawnOptions } from 'child_process'
 
-const paths = env('gocqhttp')
+function gocqhttp(options: gocqhttp.Options = {}) {
+  const args: string[] = []
+  if (options.faststart) args.push('-faststart')
+  return spawn(gocqhttp.executable, options)
+}
 
-paths.data
+namespace gocqhttp {
+  export const version = 'v1.0.0-rc3'
+
+  export interface Options extends SpawnOptions {
+    faststart?: boolean
+  }
+
+  export const extension = process.platform === 'win32' ? '.exe' : ''
+  export const executable = join(env('gocqhttp').data, version, 'go-cqhttp' + extension)
+}
+
+export = gocqhttp
