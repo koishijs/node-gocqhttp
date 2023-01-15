38ba9e443076708ed752893964aff0c82c772a40
Shigma <shigma10826@gmail.com>
2023-01-16 03:49:08 +0800

    feat: enable FORCE_TTY by default

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import { spawn, SpawnOptions } from 'child_process'
 import env from 'env-paths'
 
 function gocq(options: gocq.Options = {}) {
-  const args: string[] = []
-  if (options.faststart) args.push('-faststart')
-  return spawn(gocq.binary, args, options)
+  const args = ['-faststart']
+  return spawn(gocq.binary, args, {
+    env: {
+      FORCE_TTY: '1',
+      ...options.env,
+    },
+    ...options,
+  })
 }
 
 namespace gocq {
-  export interface Options extends SpawnOptions {
-    faststart?: boolean
-  }
+  export interface Options extends SpawnOptions {}
 
   export const version = 'v1.0.0-rc4'
   export const basename = 'go-cqhttp' + (process.platform === 'win32' ? '.exe' : '')
