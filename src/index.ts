9e6c33f78090aaced16b3f1e707cfe0f166b70a6
Shigma <shigma10826@gmail.com>
2023-03-20 13:18:08 +0800

    feat: support start options

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import { spawn, SpawnOptions } from 'child_process'
 import env from 'env-paths'
 
 function gocq(options: gocq.Options = {}) {
-  const args = ['-faststart']
+  const args: string[] = []
   if (!existsSync(gocq.binary)) {
     mkdirSync(dirname(gocq.binary), { recursive: true })
     copyFileSync(gocq.backup, gocq.binary)
   }
+  if (options.faststart) args.push('-faststart')
+  if (options.signServer) args.push('-sign-server', options.signServer)
   return spawn(gocq.binary, args, {
     env: {
       FORCE_TTY: '1',
@@ -20,7 +22,10 @@ function gocq(options: gocq.Options = {}) {
 }
 
 namespace gocq {
-  export interface Options extends SpawnOptions {}
+  export interface Options extends SpawnOptions {
+    faststart?: boolean
+    signServer?: string
+  }
 
   export const version = 'v1.0.0-rc5'
   export const runId: number = null
