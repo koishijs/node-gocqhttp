3e68443e4f68999cdf63efd8af65a78f1056ca5c
Shigma <shigma10826@gmail.com>
2023-06-27 21:00:54 +0800

    chore: bump go-cqhttp version

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ function gocq(options: gocq.Options = {}) {
     copyFileSync(gocq.backup, gocq.binary)
   }
   if (options.faststart) args.push('-faststart')
-  if (options.signServer) args.push('-sign-server', options.signServer)
+  // if (options.signServer) args.push('-sign-server', options.signServer)
   return spawn(gocq.binary, args, {
     env: {
       FORCE_TTY: '1',
@@ -24,10 +24,10 @@ function gocq(options: gocq.Options = {}) {
 namespace gocq {
   export interface Options extends SpawnOptions {
     faststart?: boolean
-    signServer?: string
+    // signServer?: string
   }
 
-  export const version = 'v1.0.1'
+  export const version = 'v1.1.0'
   export const runId: number = null
   export const basename = 'go-cqhttp' + (process.platform === 'win32' ? '.exe' : '')
   export const binary = resolve(env('gocqhttp').data, version, basename)
